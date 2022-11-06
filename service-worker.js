8158a5b369ae06b5280296ce71804448c7273621
k <karla.cahaya@gmail.com>
2022-11-07 02:31:13 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.133538abbf760e1baad584f53540109e.js"
+  "/portfolio/precache-manifest.5d6d1bdbe87d9613ff913c3e202c61b8.js"
 );
 
 workbox.clientsClaim();
