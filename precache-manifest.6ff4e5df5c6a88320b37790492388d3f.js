cc85df9d0065712cd729817382158bbb12d43ff9
k <karla.cahaya@gmail.com>
2022-11-06 16:17:33 +0700

    Updates

---
+++ precache-manifest.6ff4e5df5c6a88320b37790492388d3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5acba01750761df1b71",
+    "url": "/portfolio/static/css/main.76ed9524.chunk.css"
+  },
+  {
+    "revision": "c5acba01750761df1b71",
+    "url": "/portfolio/static/js/main.c5acba01.chunk.js"
+  },
+  {
+    "revision": "d5a82794cb252de57f41",
+    "url": "/portfolio/static/js/1.d5a82794.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "76f8395e8da6f3819f44cf0441651e55",
+    "url": "/portfolio/index.html"
+  }
+];
